dac61cc0184c9686412430a7004b1f99a7cc3159
jl <84343107+jleuth@users.noreply.github.com>
2024-11-05 18:42:32 -0600

    add in bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,6 +28,27 @@ apk add mtd-utils-ubi
 apk add bottom
 apk add neofetch
 
+# VPNOS: Install necessary packages
+echo "Updating package lists and installing required packages..."
+sudo apk add -y python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 python3-smbus i2c-tools wireguard
+pip3 install requests luma.oled
+mkdir ~/.setup
+
+# VPNOS: allow user to interface with oled hardware
+sudo usermod -a -G spi,gpio,i2c root
+
+# VPNOS: install pivpn unnattended
+cd .setup
+curl https://gist.githubusercontent.com/jleuth/0ddf047cd386deadc11c5acf90563031/raw/861f2f0674962adf860c34a48353c1da804e4cd6/vpnos.conf > vpnos.conf
+curl -L https://install.pivpn.io > install.sh
+chmod +x install.sh
+./install.sh --unattended vpnos.conf
+
+# VPNOS: Download and run the Python script to display the IP on the OLED
+echo "Running display script..."
+curl -O https://gist.githubusercontent.com/jleuth/cdd6534e4f0421750f8658df01b08615/raw/8a553b7676f7d22f4cfed2806d419f6d0182b325/displayip.py > displayip.py
+python3 displayip.py
+
 # Clear apk cache
 rm -rf /var/cache/apk/*
 
